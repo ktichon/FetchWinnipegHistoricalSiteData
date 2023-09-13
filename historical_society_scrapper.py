0b7f6ee17c27a67f0028fe049096fc30b86199cf
ktichon <ktichon@icloud.com>
2023-09-13 10:12:03 -0500

    added get_site_links

--- historical_society_scrapper.py
+++ historical_society_scrapper.py
@@ -19,11 +19,9 @@ class ManitobaHistoricalScrapper():
 
   def __init__(self):
     try:
-        self.muni = None
-        self.type = None
-        self.url = self.BASEURL + self.urlMuni + self.urlType + self.urlEND
         self.allMunicipality = []
         self.allTypes = []
+        self.allSites = []
 
 
     except Exception as error:
@@ -32,7 +30,7 @@ class ManitobaHistoricalScrapper():
   def get_all_varibles(self):
       """Gets the Municipality and site types used """
       try:
-        page = requests.get(self.url)
+        page = requests.get(self.getUrl("", ""))
         soup = BeautifulSoup(page.content, "html.parser")
         try:
           fetchedMuni = soup.find("select", id="m-name")
@@ -54,9 +52,46 @@ class ManitobaHistoricalScrapper():
             self.logger.error("ManitobaHistoricalScrapper/get_all_varibles: %s", error)
 
   def get_site_links(self):
-     """Gets all the links for the site"""
-     #for muni in self.allMunicipality:
-     #for type in self.allTypes:
+    """Gets all the links for the site"""
+    #for muni in self.allMunicipality:
+    #for type in self.allTypes:
+
+    #Temp for testing
+    siteType = "Building"
+    try:
+      url = self.getUrl("Alexander", siteType)
+      page = requests.get(url)
+      soup = BeautifulSoup(page.content, "html.parser")
+
+      try:
+         for row in soup.tbody.find_all("tr"):
+            columns = row.find_all("td")
+            siteName = row.contents[0].text
+            siteLink = row.contents[0].a["href"]
+            siteMuni = row.contents[1].text
+            siteAddress = row.contents[2].text
+            print(siteName + ", " + siteMuni + ", " + siteAddress + ", " + siteLink)
+
+            """ for column in row.contents:
+              print(column) """
+            print("\n")
+
+      except Exception as error:
+            self.logger.error("ManitobaHistoricalScrapper/get_site_links:Loop through tr %s", error)
+
+
+
+    except Exception as error:
+            self.logger.error("ManitobaHistoricalScrapper/get_site_links %s", error)
+
+  def getUrl(self, municipality, type):
+    """Returns the properly formated url"""
+    return self.BASEURL + "&m-name=" + municipality + "&st-name=" + type + "&submit=Search"
+
+
+  def fetchSiteInfo(self, siteName, siteLink, siteMuni, siteType)
+    """Gets the site information and save it to a dictionary"""
+
 
 
 
@@ -69,8 +104,9 @@ class ManitobaHistoricalScrapper():
 if __name__ == "__main__":
     siteScraper = ManitobaHistoricalScrapper()
     siteScraper.get_all_varibles()
-    print(siteScraper.allMunicipality)
-    print(siteScraper.allTypes)
+    #print(siteScraper.allMunicipality)
+    #print(siteScraper.allTypes)
+    siteScraper.get_site_links()
 
 
 
