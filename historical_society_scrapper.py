875b207c09c52fc5de1ddec1b10493193bc22072
ktichon <ktichon@icloud.com>
2023-09-13 12:21:01 -0500

    It now can fetch the site description

--- historical_society_scrapper.py
+++ historical_society_scrapper.py
@@ -30,7 +30,7 @@ class ManitobaHistoricalScrapper():
   def get_all_varibles(self):
       """Gets the Municipality and site types used """
       try:
-        page = requests.get(self.getUrl("", ""))
+        page = requests.get(self.get_url("", ""))
         soup = BeautifulSoup(page.content, "html.parser")
         try:
           fetchedMuni = soup.find("select", id="m-name")
@@ -38,7 +38,7 @@ class ManitobaHistoricalScrapper():
             if muni != "*ALL*":
               self.allMunicipality.append(muni.replace(" ", "+"))
         except Exception as error:
-            self.logger.error("ManitobaHistoricalScrapper/get_all_varibles:Get Municipality %s", error)
+            self.logger.error("ManitobaHistoricalScrapper/get_all_varibles/Get Municipality: %s", error)
 
         try:
           fetchedType = soup.find("select", id="st-name")
@@ -46,7 +46,7 @@ class ManitobaHistoricalScrapper():
             if type != "*ALL*":
               self.allTypes.append(type.replace(" ", "+"))
         except Exception as error:
-            self.logger.error("ManitobaHistoricalScrapper/get_all_varibles:Get Types %s", error)
+            self.logger.error("ManitobaHistoricalScrapper/get_all_varibles/Get Types: %s", error)
 
       except Exception as error:
             self.logger.error("ManitobaHistoricalScrapper/get_all_varibles: %s", error)
@@ -59,7 +59,7 @@ class ManitobaHistoricalScrapper():
     #Temp for testing
     siteType = "Building"
     try:
-      url = self.getUrl("Alexander", siteType)
+      url = self.get_url("Alexander", siteType)
       page = requests.get(url)
       soup = BeautifulSoup(page.content, "html.parser")
 
@@ -67,30 +67,62 @@ class ManitobaHistoricalScrapper():
          for row in soup.tbody.find_all("tr"):
             columns = row.find_all("td")
             siteName = row.contents[0].text
-            siteLink = row.contents[0].a["href"]
+            siteURL = row.contents[0].a["href"]
             siteMuni = row.contents[1].text
             siteAddress = row.contents[2].text
-            print(siteName + ", " + siteMuni + ", " + siteAddress + ", " + siteLink)
-
-            """ for column in row.contents:
-              print(column) """
-            print("\n")
+            """ print(siteName + ", " + siteMuni + ", " + siteAddress + ", " + siteURL)
+            print("\n") """
 
       except Exception as error:
-            self.logger.error("ManitobaHistoricalScrapper/get_site_links:Loop through tr %s", error)
+            self.logger.error("ManitobaHistoricalScrapper/get_site_links/Loop through tr: %s", error)
 
 
 
     except Exception as error:
             self.logger.error("ManitobaHistoricalScrapper/get_site_links %s", error)
 
-  def getUrl(self, municipality, type):
+  def get_url(self, municipality, type):
     """Returns the properly formated url"""
     return self.BASEURL + "&m-name=" + municipality + "&st-name=" + type + "&submit=Search"
 
 
-  def fetchSiteInfo(self, siteName, siteLink, siteMuni, siteType)
+  def fetch_site_info(self, siteName, siteURL, siteMuni, siteAddress, siteType):
     """Gets the site information and save it to a dictionary"""
+    try:
+      page = requests.get(siteURL)
+      soup = BeautifulSoup(page.content, "html.parser")
+      relevantData = soup.find_all("table")[0].contents[4]
+
+
+      #Getting site description
+      try:
+        siteDescription = ""
+
+        firstP = relevantData.find_all("p")[0]
+        if "Link to:" not in firstP.text:
+          siteDescription = firstP.text
+
+        for p in firstP.next_siblings:
+          if p.name != None and p.name != 'p':
+                break
+
+          if p.name == 'p':
+            text = p.text + '\n'
+
+            #Some sites didn't close the p tag, so this should cut the text off in those senerios
+            if( "\n\n" in text):
+                text = text.split("\n\n")[0]
+            siteDescription += text
+
+      except Exception as error:
+            self.logger.error("ManitobaHistoricalScrapper/fetch_site_info/Parse Description: %s", error)
+
+      #Getting site pictures
+
+
+
+    except Exception as error:
+            self.logger.error("ManitobaHistoricalScrapper/fetch_site_info: %s", error)
 
 
 
@@ -107,6 +139,7 @@ if __name__ == "__main__":
     #print(siteScraper.allMunicipality)
     #print(siteScraper.allTypes)
     siteScraper.get_site_links()
+    siteScraper.fetch_site_info("Brodie School No. 1854", "http://www.mhs.mb.ca/docs/sites/brodieschool.shtml", "Alexander", "Stead", "Building")
 
 
 
